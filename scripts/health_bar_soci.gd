02ba2be0225bc8947b1d25fc3ee2871f6df7a71e
22024-Gus <22024@nayland.school.nz>
2025-06-10 10:06:58 +1200

    1.81
    social and physical health bars now working :)

---
+++ scripts/health_bar_soci.gd
@@ -0,0 +1,19 @@
+extends TextureProgressBar
+@export var player: CharacterBody2D
+
+const BARSPEED = 2
+var current_bar_value = 100
+
+func _process(delta):
+		current_bar_value -= BARSPEED * delta
+	
+		# Don't go below zero
+		current_bar_value = max(current_bar_value, 0)
+	
+		get_node("/root/Node/CharacterBody2D/Camera2D/Health-bar-Social").value = current_bar_value
+
+func add_soci_health():
+	value += 25
+	
+	value_changed.emit()
+	current_bar_value = value
--- scripts/soci_collectable.gd
+++ scripts/soci_collectable.gd
@@ -1,12 +1,11 @@
 extends Area2D
 
-@onready var health_bar_physical: TextureProgressBar = %"Health-bar-Physical"
+@onready var health_bar_social: TextureProgressBar = %"Health-bar-Social"
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
 		animated_sprite_2d.animation = "collected"
-		health_bar_physical.add_phys_health()
+		health_bar_social.add_soci_health()
 		queue_free()
